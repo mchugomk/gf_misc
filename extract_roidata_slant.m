ba01c0bdc14003bac5737a8f11e9ad941c088553
Maureen McHugo <maureen.mchugo@gmail.com>
2021-09-13 18:35:07 -0500

    fixed

--- extract_roidata_slant.m
+++ extract_roidata_slant.m
@@ -10,12 +10,7 @@ roi_vals = roi_vals(roi_vals~=0);
 
 % Load the normalized ROI label file
 roi_info = readtable(rroi_csv,'Delimiter','comma');
-roi_info.LabelName_BrainCOLOR_=cellstr(roi_info.LabelName_BrainCOLOR_)
-roi_info.LabelName_BrainCOLOR_=categorical(roi_info.LabelName_BrainCOLOR_)
-
-% Rename rows for 3rd and 4th ventricle
-roidata=roi_info;
-return
+roi_info.LabelName_BrainCOLOR_=cellstr(roi_info.LabelName_BrainCOLOR_);
 
 
 % Check for a problem situation
@@ -33,9 +28,12 @@ roidata = table();
 for r = 1:height(roi_info)
 	voxelinds = Yroi(:)==roi_info.LabelNumber_BrainCOLOR_(r);
 	voxeldata = Yfmri(:,voxelinds);
-	roidata.(roi_info.LabelName_BrainCOLOR_{r})(:,1) = mean(voxeldata,2);
+% 	roidata.(roi_info.LabelName_BrainCOLOR_{r})(:,1) = mean(voxeldata,2);
+    roidata.LabelName{r} = roi_info.LabelName_BrainCOLOR_{r};
+    roidata.LabelNumber(r) = roi_info.LabelNumber_BrainCOLOR_(r);
+    roidata.LabelMean(r) = mean(voxeldata,2);
 end
-% roidata.Properties.VariableNames = roi_info.LabelName_BrainCOLOR_(:)';
+
 
 % Save ROI data to file
 roidata_csv = [out_dir '/roidata_' tag '.csv'];
